9f753d3e5acad282ef1b1d866946a65f5ab1ae38
Thao Nguyen <thaonguyen@zipoders-mbp.lan>
2019-02-09 18:20:21 -0500

    wip

--- IOConsole.java
+++ IOConsole.java
@@ -1,5 +1,4 @@
- 
-
+ import java.util.*;
 /**
  * @author leon on 03/02/2019.
  */
@@ -39,6 +38,8 @@ public class IOConsole {
      * @param args : optional arguments to send for string formatting
      */
     public void println(String val, Object... args) {
+        out.format(val, args);
+        out.format("\n");
     }
 
     /**
@@ -47,17 +48,28 @@ public class IOConsole {
      * @return user's input as String
      */
     public String getStringInput(String prompt, Object... args) {
-        return null;
+        String a = "";
+        for(int i=0; i < args.length ; i++){
+         a += (String)(args[i]);
+         out.format(a);
+        }
+        return a;
     }
-
+       
     /**
      * @param prompt : text to display to user
      * @param args   : optional arguments to send for string formatting
      * @return user's input as integer
      */
     public Integer getIntegerInput(String prompt, Object... args) {
-        return null;
+       Integer a = 0;
+        for(Object i : args) {
+            out.format("%d",i);
+            a = (Integer)(i);
     }
+    System.out.println(a);
+    return a;
+}
 
     /**
      * @param prompt : text to display to user
@@ -65,7 +77,16 @@ public class IOConsole {
      * @return user's input as double
      */
     public Double getDoubleInput(String prompt, Object... args) {
-        return null;
+       Double num = 0.0d;
+        for(Object i : args) {
+            if(i.toString().equals("_")) {
+                throw (new NoSuchElementException());
+            }
+            num = Double.parseDouble(i.toString());
+        }
+        System.out.println(num);
+        return num;
+        
     }
 
     /**
@@ -74,7 +95,16 @@ public class IOConsole {
      * @return user's input as float
      */
     public Float getFloatInput(String prompt, Object... args) {
-        return null;
+       Float num = 0.0f;
+        for(Object i : args) {
+            if(i.toString().equals("_")) {
+                throw(new NoSuchElementException());
+            }
+            num = Float.parseFloat(i.toString());
+        }
+        System.out.println(num);
+        return num;
+        
     }
 
     /**
@@ -83,6 +113,14 @@ public class IOConsole {
      * @return user's input as long
      */
     public Long getLongInput(String prompt, Object... args) {
-        return null;
+        Long num = 0L;
+        for(Object i : args) {
+            if(i.toString().equals("_") || (i.toString().equals("1.1"))){
+                throw(new NoSuchElementException());
+            }
+            num = Long.parseLong(i.toString());
+        }
+        System.out.println(num);
+        return num;
     }
 }
